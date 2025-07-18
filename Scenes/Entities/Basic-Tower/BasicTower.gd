dd19ffcb5b1b0966671df778b0833ee0ce2dcb21
Oliwier Matus <matusoliwier@gmail.com>
2025-07-18 16:07:37 +0200

    add basic tower class

--- Scenes/Entities/Basic-Tower/BasicTower.gd
+++ Scenes/Entities/Basic-Tower/BasicTower.gd
@@ -1,5 +1,7 @@
 extends Node
 
+class_name BasicTower
+
 @export var attack_speed : int
 @export var attack_range_radius : int
 @export var attack_damage : int
@@ -25,7 +27,7 @@ func _ready() -> void:
 	visible_attack_area.visible = is_attack_radious_visible
 	attack_area.radius = attack_range_radius
 	attack_timer.wait_time = attack_speed
-	
+
 func _on_signal_toggle_attack_area_visible():
 	is_attack_radious_visible = !is_attack_radious_visible
 	visible_attack_area.visible = is_attack_radious_visible
@@ -33,38 +35,40 @@ func _on_signal_toggle_attack_area_visible():
 func _process(delta: float) -> void:
 	if enemies_in_range.size() == 0:
 		return
-	var target : Node2D
 	if attack_timer.is_stopped():
-		match attack_enemy_select:
-			enemy_select_types.FIRST_ENEMY:
-				var min_distance : int = -1
-				for x in enemies_in_range:
-					if min_distance > x.distance_to_base || min_distance == -1:
-						min_distance = x.distance_to_base
-						target = x
-			enemy_select_types.LAST_ENEMY:
-				var max_distance : int = -1
-				for x in enemies_in_range:
-					if max_distance < x.distance_to_base || max_distance == -1:
-						max_distance = x.distance_to_base
-						target = x
-			enemy_select_types.MOST_HEALTH:
-				var max_health : int = -1
-				for x in enemies_in_range:
-					if max_health < x.health || max_health == -1:
-						max_health = x.health
-						target = x
-			enemy_select_types.LEAST_HEALTH:
-				var max_health : int = -1
-				for x in enemies_in_range:
-					if max_health > x.health || max_health == -1:
-						max_health = x.health
-						target = x
-		# samo strzelanie nie wybieranie przeciwnika
-		target.take_damage(attack_damage)
-		tower_gun_hinge.look_at(target.position)
-		attack_timer.start()
-
+		aim_to_target()
+		
+func aim_to_target():
+	var target : Node2D
+	match attack_enemy_select:
+		enemy_select_types.FIRST_ENEMY:
+			var min_distance : int = -1
+			for x in enemies_in_range:
+				if min_distance > x.distance_to_base || min_distance == -1:
+					min_distance = x.distance_to_base
+					target = x
+		enemy_select_types.LAST_ENEMY:
+			var max_distance : int = -1
+			for x in enemies_in_range:
+				if max_distance < x.distance_to_base || max_distance == -1:
+					max_distance = x.distance_to_base
+					target = x
+		enemy_select_types.MOST_HEALTH:
+			var max_health : int = -1
+			for x in enemies_in_range:
+				if max_health < x.health || max_health == -1:
+					max_health = x.health
+					target = x
+		enemy_select_types.LEAST_HEALTH:
+			var max_health : int = -1
+			for x in enemies_in_range:
+				if max_health > x.health || max_health == -1:
+					max_health = x.health
+					target = x
+	# samo strzelanie nie wybieranie przeciwnika
+	target.take_damage(attack_damage)
+	tower_gun_hinge.look_at(target.position)
+	attack_timer.start()
 
 func _on_attack_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Enemy"):
