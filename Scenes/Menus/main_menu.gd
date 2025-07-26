a376e02f86ff52e2e09ec87b19290e54587fe8cb
piotr rotek <piotr.roterman@gmail.com>
2025-07-26 12:42:18 +0200

    This commit will: add background music

--- Scenes/Menus/main_menu.gd
+++ Scenes/Menus/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
-
+func _ready() -> void:
+	Audio.play_music()
+	
 
 func _on_start_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menus/LevelsMap.tscn")
