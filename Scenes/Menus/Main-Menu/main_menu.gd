4b4eeb7d8dae02e28cd7614208f496e574089295
Oliwier Matus <matusoliwier@gmail.com>
2025-10-17 18:23:18 +0200

    fix options button

--- Scenes/Menus/Main-Menu/main_menu.gd
+++ Scenes/Menus/Main-Menu/main_menu.gd
@@ -11,8 +11,16 @@ func _on_start_game_button_pressed() -> void:
 	get_tree().change_scene_to_packed(LEVELS_MAP)
 
 
-func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_packed(OPTIONS)
+func _on_options_pressed() -> void:
+	var current_scene_path = get_tree().current_scene.scene_file_path
+	var current_packed_scene = load(current_scene_path)
+	
+	var options_instance = OPTIONS.instantiate()
+	options_instance.previous_scene = current_packed_scene
+	
+	get_tree().root.add_child(options_instance)
+	get_tree().current_scene.queue_free()
+	get_tree().current_scene = options_instance
 
 
 func _on_exit_button_pressed() -> void:
--- Scenes/Menus/Options/options.gd
+++ Scenes/Menus/Options/options.gd
@@ -1,6 +1,6 @@
 extends Control
-var MAIN_MENU = load("res://Scenes/Menus/Main-Menu/MainMenu.tscn")
+@export var previous_scene: PackedScene
 
 func _on_options_panel_exit_options() -> void:
-	get_tree().change_scene_to_packed(MAIN_MENU)
+	get_tree().change_scene_to_packed(previous_scene)
 	pass # Replace with function body.
